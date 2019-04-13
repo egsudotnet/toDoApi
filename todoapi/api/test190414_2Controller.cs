5ec88c2b5384c7d14249f576e9b535abdc865633
Egih <egsudotnet@gmail.com>
2019-04-14 06:27:14 +0700

    test190414_2

---
+++ todoapi/api/test190414_2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace todoapi.api
+{
+    public class test190414_2Controller : Controller
+    {
+        // GET: test190414_2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
