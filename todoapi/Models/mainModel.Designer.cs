70c3ba6bbf1142df3d111a517e1694f8701729d3
Egih <egihsugiatna@gmail.com>
2019-04-14 09:59:34 +0700

    tes2

--- todoapi/Models/mainModel.Designer.cs
+++ todoapi/Models/mainModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Users\LENOVO\source\repos\todoapi\todoapi\Models\mainModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\LENOVO\Source\Repos\egsudotnet\toDoApi190414\todoapi\Models\mainModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
