8f7b6fdf5061201e0f60aca82686ae368abf51a6
egih <egsudotnet@gmail.com>
2019-04-17 12:57:12 +0700

    333

---
+++ todoapi/api/test190414_SSH_3 - Copy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace todoapi.api
+{
+    public class test190414_1Controller : Controller
+    {
+        // GET: test190414_1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
